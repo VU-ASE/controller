76e33468fc2da50fe43b516fd6ef0559a126c020
Leo <leonidivanovzalgo@gmail.com>
2025-06-05 11:39:16 +0200

    feat: add unit tests for calculating steer value

--- src/main.go
+++ src/main.go
@@ -20,6 +20,55 @@ func floatEqual(a, b float64) bool {
 	return a-b < 0.0001 && b-a < 0.0001
 }
 
+func initializePIDController(kp, ki, kd float64) pid.Controller {
+	result := pid.Controller{
+		Config: pid.ControllerConfig{
+			ProportionalGain: float64(kp),
+			IntegralGain:     float64(ki),
+			DerivativeGain:   float64(kd),
+		},
+	}
+	return result
+}
+
+func calculateSteerValue(pidController pid.Controller, trajectoryPoints []*pb_outputs.CameraSensorOutput_Trajectory_Point, desiredTrajectoryPoint int) float64 {
+	// This is the middle of the longest consecutive slice, it should be in the middle of the image (horizontally)
+	firstPoint := trajectoryPoints[0]
+
+	// Use the PID controller to decide where to go
+	pidController.Update(pid.ControllerInput{
+		ReferenceSignal:  float64(desiredTrajectoryPoint),
+		ActualSignal:     float64(firstPoint.X),
+		SamplingInterval: 100 * time.Millisecond,
+	})
+	steerValue := pidController.State.ControlSignal
+	// min-max
+	if steerValue > 1 {
+		steerValue = 1
+	} else if steerValue < -1 {
+		steerValue = -1
+	}
+	return -steerValue // has to be inverted
+}
+
+func sendOutput(actuatorOutput *roverlib.WriteStream, speed float64, steerValue float64) error {
+	err := actuatorOutput.Write(
+		&pb_outputs.SensorOutput{
+			SensorId:  2,
+			Timestamp: uint64(time.Now().UnixMilli()),
+			SensorOutput: &pb_outputs.SensorOutput_ControllerOutput{
+				ControllerOutput: &pb_outputs.ControllerOutput{
+					SteeringAngle: float32(steerValue),
+					LeftThrottle:  float32(speed),
+					RightThrottle: float32(speed),
+					FrontLights:   false,
+				},
+			},
+		},
+	)
+	return err
+}
+
 func run(
 	service roverlib.Service, config *roverlib.ServiceConfiguration) error {
 
@@ -64,14 +113,7 @@ func run(
 	}
 	desiredTrajectoryPoint := int(desiredTrajectoryPointFloat)
 
-	// Initialize pid controller
-	pidController = pid.Controller{
-		Config: pid.ControllerConfig{
-			ProportionalGain: float64(kp),
-			IntegralGain:     float64(ki),
-			DerivativeGain:   float64(kd),
-		},
-	}
+	pidController = initializePIDController(kp, ki, kd)
 
 	// Main loop, subscribe to trajectory data and send decision data
 	for {
@@ -108,14 +150,7 @@ func run(
 			kd = newKd
 			ki = newKi
 
-			// Create a new PID controller with the new values
-			pidController = pid.Controller{
-				Config: pid.ControllerConfig{
-					ProportionalGain: float64(kp),
-					IntegralGain:     float64(ki),
-					DerivativeGain:   float64(kd),
-				},
-			}
+			pidController = initializePIDController(kp, ki, kd)
 
 			log.Info().Float64("kp", kp).Float64("kd", kd).Float64("ki", ki).Msg("Updated PID controller")
 		}
@@ -126,38 +161,11 @@ func run(
 			log.Warn().Msg("Received sensor data that had no trajectory points")
 			continue
 		}
-		// This is the middle of the longest consecutive slice, it should be in the middle of the image (horizontally)
-		firstPoint := trajectoryPoints[0]
 
-		// Use the PID controller to decide where to go
-		pidController.Update(pid.ControllerInput{
-			ReferenceSignal:  float64(desiredTrajectoryPoint),
-			ActualSignal:     float64(firstPoint.X),
-			SamplingInterval: 100 * time.Millisecond,
-		})
-		steerValue := pidController.State.ControlSignal
-		// min-max
-		if steerValue > 1 {
-			steerValue = 1
-		} else if steerValue < -1 {
-			steerValue = -1
-		}
-		steerValue = -steerValue
+		steerValue := calculateSteerValue(pidController, trajectoryPoints, desiredTrajectoryPoint)
 
-		err = actuatorOutput.Write(
-			&pb_outputs.SensorOutput{
-				SensorId:  2,
-				Timestamp: uint64(time.Now().UnixMilli()),
-				SensorOutput: &pb_outputs.SensorOutput_ControllerOutput{
-					ControllerOutput: &pb_outputs.ControllerOutput{
-						SteeringAngle: float32(steerValue),
-						LeftThrottle:  float32(speed),
-						RightThrottle: float32(speed),
-						FrontLights:   false,
-					},
-				},
-			},
-		)
+		err = sendOutput(actuatorOutput, speed, steerValue)
+
 		// Send it for the actuator (and others) to use
 		if err != nil {
 			log.Err(err).Msg("Failed to send controller output")
